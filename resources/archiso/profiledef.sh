e61ebed1fbae1dc123317e102d6d53b44d0530ed
Andrew <kezin636@gmail.com>
2025-10-24 02:23:37 +0300

    branding and stuff + removed syslinux boot option, thus no BIOS, now it's UEFI only.

--- resources/archiso/profiledef.sh
+++ resources/archiso/profiledef.sh
@@ -8,8 +8,7 @@ iso_application="Project GDL Live/Rescue DVD"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux'
-           'uefi.systemd-boot')
+bootmodes=('uefi.systemd-boot')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
