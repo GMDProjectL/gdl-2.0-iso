e1363e92ee75ba372f9552e071b73b9ce725d55f
Andrew <kezin636@gmail.com>
2025-10-23 10:28:27 +0300

    ARCH_CONTAINER_NAME is now a var in build.yml

--- src/prepare_environment.sh
+++ src/prepare_environment.sh
@@ -5,8 +5,6 @@ sudo apt-get install -y podman
 
 curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh
 
-ARCH_CONTAINER_NAME=gdl
-
 distrobox create --root --image archlinux --name ${ARCH_CONTAINER_NAME} -Y
 
 distrobox enter --root ${ARCH_CONTAINER_NAME} -- \
