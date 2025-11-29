b09f3b6f2719235aa6cad4bf8f03cad63c043168
Andrew <kezin636@gmail.com>
2025-11-29 04:35:39 +0300

    braindead

--- src/installer.py
+++ src/installer.py
@@ -14,7 +14,7 @@ class Installer(Archiso):
     
     def clone_installer(self):
         loguru.logger.info("Cloning the installer...")
-        return os.system(f'git clone {self.installer_dir_location}') == 0
+        return os.system(f'git clone https://github.com/GMDProjectL/gdl-2.0-installer {self.installer_dir_location}') == 0
     
     def create_gui_autostart(self):
         autostart_dir = f'{self.target_iso_dir}/airootfs/etc/skel/.config/autostart'
